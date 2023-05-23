a225a963bbd2ca7c3c484f75c5e993e533b9ed43
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-05-23 19:16:39 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ app.use(cors());
  
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
