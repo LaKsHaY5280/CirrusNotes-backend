1aee8ba619814c47cc3ff34662d6c4cf56d72c18
LaKsHaY5280 <130125619+LaKsHaY5280@users.noreply.github.com>
2023-05-23 19:31:42 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
+const path = require("path");
 const connectToMongo = require("./db");
 var cors = require("cors");
- 
+
 const app = express();
-app.use(express.static("public")); 
+app.use(express.static("public"));
 
 const port = 5000;
 
-app.use(cors()); 
- 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
